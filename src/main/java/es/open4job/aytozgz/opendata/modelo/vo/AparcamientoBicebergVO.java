e1d99281371aaa7694dc0c74b54e4fccb94ea1a6
lchacon <lchaconperez@gmail.com>
2015-01-26 13:28:05 +0100

    Completada clase AparcamientoBicebergVO

--- src/main/java/es/open4job/aytozgz/opendata/modelo/vo/AparcamientoBicebergVO.java
+++ src/main/java/es/open4job/aytozgz/opendata/modelo/vo/AparcamientoBicebergVO.java
@@ -1,5 +1,21 @@
 package es.open4job.aytozgz.opendata.modelo.vo;
 
 public class AparcamientoBicebergVO {
-
+	int id;
+	double latitud;
+	double longitud;
+	String icon;
+	String title;
+	String link;
+	
+	public AparcamientoBicebergVO(int id, double latitud, double longitud,
+			String icon, String description, String link) {
+		super();
+		this.id = id;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.icon = icon;
+		this.title = description;
+		this.link= link;
+	}
 }
