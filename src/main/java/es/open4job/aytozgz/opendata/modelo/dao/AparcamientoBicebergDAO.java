6fb23de1f0eed8f8313dbd065320348334991aad
lchacon <lchaconperez@gmail.com>
2015-01-27 10:02:06 +0100

    Rellenado DAO, prueba para que Carlos se entere

--- src/main/java/es/open4job/aytozgz/opendata/modelo/dao/AparcamientoBicebergDAO.java
+++ src/main/java/es/open4job/aytozgz/opendata/modelo/dao/AparcamientoBicebergDAO.java
@@ -1,5 +1,14 @@
 package es.open4job.aytozgz.opendata.modelo.dao;
+import java.util.*;
+
+import es.open4job.aytozgz.opendata.modelo.vo.AparcamientoBicebergVO;
 
 public class AparcamientoBicebergDAO {
-
+	
+	List<AparcamientoBicebergVO> getLstAparcamientoBiceberg(){
+		List<AparcamientoBicebergVO> l=null;
+		
+		
+		return l;
+	}
 }
